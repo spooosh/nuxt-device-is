2971f72aea36c3dd4744fd2db4baf1645b9ad476
spooosh <sp@idaproject.com>
2023-07-20 23:53:05 +0300

    chore(release): v1.2.3
    Фикс Safari:
    - Поменял элемент для вычисления устройства на клиента

--- test/fixture/pages/index.vue
+++ test/fixture/pages/index.vue
@@ -1,27 +1,46 @@
 <template>
-    <div v-if="$deviceIs.mobile">
-        Is Mobile
-    </div>
+  <div v-if="$deviceIs.mobile">
+    Is Mobile
+  </div>
 
-    <div v-else-if="$deviceIs.tablet">
-        Is tablet
-    </div>
+  <div v-else-if="$deviceIs.tablet">
+    Is tablet
+  </div>
 
-    <div v-else-if="$deviceIs.laptop">
-        Is laptop && PC
-    </div>
+  <div v-else-if="$deviceIs.laptop">
+    Is laptop && PC
+  </div>
 
-    <div v-else-if="$deviceIs.desktop">
-        Is desktop && PC
-    </div>
+  <div v-else-if="$deviceIs.desktop">
+    Is desktop && PC
+  </div>
 
   <div v-else-if="$deviceIs.crawler">
-        crawler
-    </div>
+    crawler
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'Index'
-    };
+export default {
+  name: 'Index',
+
+  mounted() {
+    window.addEventListener('resize', () => {
+      console.log('window.innerWidth - ' + window.innerWidth);
+      console.log('documentEL.clientWidth - ' + document.documentElement.clientWidth);
+    });
+  },
+};
 </script>
+
+<style lang="css">
+body {
+  height: 2000px;
+}
+
+@media (max-width: 1440px) {
+  body {
+    color: red;
+  }
+}
+</style>
